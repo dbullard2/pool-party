12d549ad6d1ad09193cc76cef08e4f349e78e622
dbullard2 <daniel.earl.bullard@gmail.com>
2020-10-14 08:45:19 -0500

    bug fixes

--- js/script.js
+++ js/script.js
@@ -2,5 +2,7 @@ var hero = document.getElementById('hero');
 var images = ['hero1', 'hero2', 'hero3', 'hero4', 'hero5'];
 
 hero.style.backgroundImage =
-  "url('../img/" + images[Math.floor(Math.random() * images.length)] + ".jpg')";
+  "url('https://raw.githubusercontent.com/dbullard2/pool-party/master/img/" +
+  images[Math.floor(Math.random() * images.length)] +
+  ".jpg')";
 console.log(test);
