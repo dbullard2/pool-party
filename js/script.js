fb309869c69a893c5c3a8ef3fa2a36419a7091ae
dbullard2 <daniel.earl.bullard@gmail.com>
2020-10-14 23:47:44 -0500

    swimwear.html - done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 var hero = document.getElementById('hero');
 var images = ['hero1', 'hero2', 'hero3', 'hero4', 'hero5'];
-
+var category = document.getElementsByClassName('category')[0];
 hero.style.backgroundImage =
   "url('https://raw.githubusercontent.com/dbullard2/pool-party/master/img/" +
   images[Math.floor(Math.random() * images.length)] +
   ".jpg')";
+
+//filter items by shopping category
+filterSelection('all');
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName('canHide');
+  if (c == 'all') {
+    c = '';
+    category.innerHTML = 'All';
+  } else if (c == 0) {
+    category.innerHTML = 'Mens';
+  } else if (c == 1) {
+    category.innerHTML = 'Womens';
+  }
+
+  for (i = 0; i < x.length; i++) {
+    x[i].classList.add('is-hidden');
+    if (x[i].className.indexOf(c) > -1) {
+      x[i].classList.remove('is-hidden');
+    }
+  }
+}
